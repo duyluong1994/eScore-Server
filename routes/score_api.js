9ba576f8f3bc8e5f36576dc1446c176dc81adca6
Lupo Solitario <a24135@thanglong.edu.vn>
2018-03-22 22:21:13 +0700

    up

--- routes/score_api.js
+++ routes/score_api.js
@@ -1,20 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var cheerio = require('cheerio');
+var webdriver = require('selenium-webdriver');
 
 
 
 // GET todayMatch
 router.get('/',function (req, res) {
-    /*const {Builder, until} = require('selenium-webdriver');
-    let driver = new Builder()
-        .forBrowser('firefox')
-        .usingServer( 'http://localhost:8080/wd/hub')
+    //const {Builder, until} = require('selenium-webdriver');
+    let driver = new webdriver.Builder()
+        .forBrowser('phantomjs')
         .build();
     const hltv_url = 'https://www.hltv.org/';
 
     driver.get(hltv_url)
-        .then(() => driver.wait(until.titleIs('CS:GO News & Coverage | HLTV.org'), 1000))
+        .then(() => driver.wait(function() {
+            return driver.getTitle().then(function(title) {
+                console.log(title);
+                return title === 'CS:GO News & Coverage | HLTV.org';
+            });
+        }, 5000))
         .then(() => driver.executeScript("window.scrollTo(0, document.body.scrollHeight);"))
         .then(() => driver.getPageSource())
         .then((source) => {
@@ -26,13 +31,14 @@ router.get('/',function (req, res) {
             console.log(items[0]);
             console.log(items[0].html());
             console.log(items[0].text());
+            console.log($.html());
             //Do whatever you want with the result
 
             //console.log(item.html());
         })
         .then(() => {
             driver.quit();
-        });*/
+        });
     res.render('pages/score_api');
 });
 module.exports = router;
