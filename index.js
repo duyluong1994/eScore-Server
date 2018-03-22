93cc9b01af6965c3e164c80560d656d24331ca92
Duy Luong <duyluong1994@gmail.com>
2018-03-22 17:51:42 +0700

    new_way

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var score_api = require('./routes/score_api');
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+    .use('/score_api', score_api)
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ routes/score_api.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var cheerio = require('cheerio');
+
+
+
+// GET todayMatch
+router.get('/',function (req, res) {
+    const {Builder, until} = require('selenium-webdriver');
+    let driver = new Builder()
+        .forBrowser('firefox')
+        .usingServer( 'http://localhost:8080/wd/hub')
+        .build();
+    const hltv_url = 'https://www.hltv.org/';
+
+    driver.get(hltv_url)
+        .then(() => driver.wait(until.titleIs('CS:GO News & Coverage | HLTV.org'), 1000))
+        .then(() => driver.executeScript("window.scrollTo(0, document.body.scrollHeight);"))
+        .then(() => driver.getPageSource())
+        .then((source) => {
+            const $ = cheerio.load(source);
+            var items = [];
+            $('.top-border-hide').find(".hotmatch-box.a-reset").each((_,ele) => {
+                items.push($(ele));
+            });
+            console.log(items[0]);
+            console.log(items[0].html());
+            console.log(items[0].text());
+            //Do whatever you want with the result
+
+            //console.log(item.html());
+        })
+        .then(() => {
+            driver.quit();
+        });
+
+    res.send();
+});
+module.exports = router;
