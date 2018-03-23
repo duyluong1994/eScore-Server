5224d4b9359d3f1ef30e20d8182fb5e832571cc2
Lupo Solitario <a24135@thanglong.edu.vn>
2018-03-24 02:47:39 +0700

    test

--- routes/score_api.js
+++ routes/score_api.js
@@ -1,49 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var cheerio = require('cheerio');
-/*chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil);
-chrome_opts = chrome_bin ? { "chromeOptions" : { "binary" : chrome_bin } } : {};*/
+const puppeteer = require('puppeteer');
 
 
 
 // GET todayMatch
 router.get('/',function (req, res) {
-  /*  const {Builder, until} = require('selenium-webdriver');
-    let driver = new Builder()
-        .forBrowser('chrome')
-        //
-        .build();*/
-    var webdriver = require('selenium-webdriver');
+    function timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-    var chromeCapabilities = webdriver.Capabilities.chrome();
-//setting chrome options to start the browser fully maximized
-    var chromeOptions = {
-        'args': ['--test-type', '--start-maximized']
-    };
-    chromeCapabilities.set('chromeOptions', chromeOptions);
-    var driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
-    const hltv_url = 'https://www.hltv.org/';
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.hltv.org/');
+        await timeout(1000);
+        const matches = await page.evaluate(() => [...document.querySelectorAll('.teambox.match')].map(elem => elem.innerHTML));
 
-    driver.get(hltv_url)
-        .then(() => driver.wait(until.titleIs('CS:GO News & Coverage | HLTV.org'), 1000))
-        .then(() => driver.executeScript("window.scrollTo(0, document.body.scrollHeight);"))
-        .then(() => driver.getPageSource())
-        .then((source) => {
-            const $ = cheerio.load(source);
-            var items = [];
-            $('.top-border-hide').find(".hotmatch-box.a-reset").each((_,ele) => {
-                items.push($(ele));
-            });
-            console.log(items[0]);
-            console.log(items[0].html());
-            console.log(items[0].text());
-            //Do whatever you want with the result
+        console.log(matches);
 
-            //console.log(item.html());
-        })
-        .then(() => {
-            driver.quit();
-        });
+        await browser.close();
+    })();
     res.render('pages/score_api');
 });
 module.exports = router;
