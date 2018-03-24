4b08661a348485cf564d9e62474e733ea72a250c
Lupo Solitario <a24135@thanglong.edu.vn>
2018-03-24 15:58:13 +0700

    test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 var score_api = require('./routes/score_api');
 
 var https = require("https");
 setInterval(function() {
-    https.get("https://escore-server.herokuapp.com/");
-}, 50000); // ping server every 30s
+    https.get("https://escore-server.herokuapp.com/score_api");
+}, 50000); // ping server to get data every 30s
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -15,4 +15,4 @@ express()
   .set('view engine', 'ejs')
     .use('/score_api', score_api)
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
