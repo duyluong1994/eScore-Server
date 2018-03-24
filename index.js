69fd62993779ece1eeff0cb13c11ac638ce654cb
Lupo Solitario <a24135@thanglong.edu.vn>
2018-03-24 14:24:23 +0700

    add ping each 30s

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 5000
 
 var score_api = require('./routes/score_api');
 
+var http = require("http");
+setInterval(function() {
+    http.get("https://escore-server.herokuapp.com/");
+}, 50000); // every 5 minutes (300000)
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
